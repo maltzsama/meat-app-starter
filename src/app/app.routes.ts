72ed94c2b03bfea902c90c38855e6c8ad3e6d1b5
Demetrius Albuquerque <causbla@gmail.com>
2017-12-23 00:33:38 -0400

    added router component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+];
+
