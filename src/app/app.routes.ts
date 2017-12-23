72215e576fe2f633f979306d054a83fc7d37c261
Demetrius Albuquerque <causbla@gmail.com>
2017-12-23 04:36:02 -0400

    added detail of restaurant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
+import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
+
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'restaurants', component: RestaurantsComponent}
+    { path: 'restaurants', component: RestaurantsComponent},
+    { path: 'restaurants/:id', component: RestaurantDetailComponent}
 ];
-
