35917cf76509a385d49834eda66e22f9e4c1d134
Demetrius Albuquerque <causbla@gmail.com>
2017-12-23 04:01:35 -0400

    added function http to api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -12,6 +11,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
+
 
 @NgModule({
   declarations: [
