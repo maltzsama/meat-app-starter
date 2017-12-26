663d2fe89404dd3de40f62b6fa435aeec4a1c3bf
Demetrius Albuquerque <causbla@gmail.com>
2017-12-26 16:02:14 -0400

    added data to main form

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,23 +1,46 @@
 import { RadioOption } from './radio-option.model';
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[];
   value: any;
+  onChange: any;
+  onTapped: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  setValue(value: any) {
+  setValue(value: any): void {
     this.value = value;
+    this.onChange(value);
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+    ;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(value: any): void {
+    this.onTapped = value;
   }
 
 }
